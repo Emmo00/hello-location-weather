107b4e06c87e0b4bbf569c97e029b89931986da8
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2024-07-04 12:20:31 +0100

    next

--- app.js
+++ app.js
@@ -2,20 +2,25 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res) => {
-  console.log(req.method, req.url, req.ip, req.ips);
+const getClientIp = (req) => {
+  const ipAddress = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  return ipAddress.includes("::ffff:") ? ipAddress.split(":").pop() : ipAddress;
+};
+
+app.use((req, res, next) => {
+  console.log(req.method, req.url, getClientIp(req));
+  next();
 });
 
 app.get("/api/hello", async (req, res) => {
   const { visitor_name } = req.query;
-  const user_ip = req.ip;
+  const user_ip = getClientIp(req);
 
   // get location from ip
   const location_api_url = `http://ip-api.com/json/${user_ip}`;
   const location_response = await fetch(location_api_url);
 
   const location_response_json = await location_response.json();
-  console.log(location_response_json);
   const user_city = location_response_json.city;
   const user_longitude = location_response_json.lon;
   const user_latitude = location_response_json.lat;
@@ -24,7 +29,6 @@ app.get("/api/hello", async (req, res) => {
   const weather_api_url = `https://api.open-meteo.com/v1/forecast?latitude=${user_latitude}&longitude=${user_longitude}&current=temperature_2m`;
   const weather_response = await fetch(weather_api_url);
   const weather_response_json = await weather_response.json();
-  console.log(weather_response_json);
   const location_temperature = weather_response_json.current.temperature_2m;
 
   return res.json({
